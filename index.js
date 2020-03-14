00fb3570924c6a5e86121afe8fb44e04fff4710f
Green Wang <cocowool@gmail.com>
2020-03-14 19:10:14 +0800

    make it work

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 'use strict';
 
+// match markdown image and covert to asset_img 
 hexo.extend.filter.register('before_post_render', function(data){
 
+    data.content = data.content.replace(/!{1}\[([^\[\]]*)\]\((\S*)\s?(?:".*")?\)/g,
+        function(match_str, label, path){
+
+            // if only one /
+            if( (path.split("/")).length == 2){
+                return "{% asset_img " + (path.split("/"))[1] + " " +  label + " %}" 
+            }else{
+                return match_str;
+            }
+
+        });
+
+    return data;
 });
